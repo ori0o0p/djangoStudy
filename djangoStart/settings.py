78e9eed230798ae457744ee8999065d53cfa6788
101 <tmddnjs001133@dsm.hs.kr>
2024-01-16 21:37:18 +0900

    db 연결

--- djangoStart/settings.py
+++ djangoStart/settings.py
@@ -55,8 +55,10 @@ WSGI_APPLICATION = 'djangoStart.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'djongo',
+        'NAME': 'practice_django',
+        'HOST': '127.0.0.1',
+        'PORT': 27017,
     }
 }
 
