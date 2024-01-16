a0d2d3d375a0ee68ad97681365205130482136ba
101 <tmddnjs001133@dsm.hs.kr>
2024-01-16 21:13:17 +0900

    게시물 생성 로직

--- articleApp/views.py
+++ articleApp/views.py
@@ -1,6 +1,18 @@
 from django.http import HttpResponse
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404, redirect
+
+from articleApp.models import Article
+
 
 # Create your views here.
 def hello_world(request):
     return HttpResponse("hello world!")
+
+def create_article(request):
+    if request.method == 'POST':
+        title = request.POST['title']
+        content = request.POST['content']
+        article = Article(title=title, content=content)
+        article.save()
+        return HttpResponse("게시물 생성완료")
+    return render(request, '')
