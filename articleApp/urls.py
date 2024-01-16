798a3f8564777174368335ef7885ea4b4a2a089a
101 <tmddnjs001133@dsm.hs.kr>
2024-01-16 21:17:56 +0900

    게시물 생성

--- articleApp/urls.py
+++ articleApp/urls.py
@@ -3,6 +3,6 @@ from django.urls import path
 from articleApp import views
 
 urlpatterns = [
-    path('articles/', views.article_list, name='article_list')
-    
+    path('articles/', views.article_list, name='article_list'),
+    path('articles/create/', views.create_article, name='create_article'),
 ]
